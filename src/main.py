151f1e51b4aac51550e81d5b41f1ce1f3b68725c
Lucas de Miranda Bastos <lucas@kunumi.ai>
2017-10-16 21:16:46 -0200

    Finishing doc

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
 import operator
+import sys
 
 from political_problem import read_graph_from_file
 from political_problem import normalize_graph
 from political_problem import shortest_path
 from political_problem import calculate_betweenness
 
-graph = read_graph_from_file("../examples/Ex5.txt")
+graph = read_graph_from_file(sys.argv[1])
 normalized_graph = normalize_graph(graph)
 
-# TODO -- in case of draw, output must be ordered by key
-
 betweenness = calculate_betweenness(normalized_graph)
+
+# Sorting
 sorted_betweenness = sorted(betweenness.items(), key=operator.itemgetter(1))
 sorted_betweenness = [x for x,y in sorted_betweenness]
 sorted_betweenness.reverse()
 
-print(betweenness)
-print(sorted_betweenness)
+#Print result to file
+output_fp = open(sys.argv[2], 'w')
+output_list = map(str, sorted_betweenness) # Convert to str
+output_fp.write(",".join(output_list))
+output_fp.write("\n")
+output_fp.close()
--- src/political_problem/graph/algorithms.py
+++ src/political_problem/graph/algorithms.py
@@ -1,33 +1,23 @@
-import math
-
 from .graph import Graph
 from .graph_heap import GraphHeap
 
 # The problem will be solved using betweenness algorithm that uses shortest path
 # algorithm. In order to use shortest path, every edge should be converted as
 # following:
-#     w' = log(100 - w + C)
+#     w' = 100 - w 
 #  Where:
 #    w' is the new edge weight
 #    w is the old edge weight
-#    C is approximately 2.23e-308. A very low number used just to avoid log(0)
-# situation.
-
+#
 # More details can be found in documentation
+
 def normalize_graph(graph):
-    min_float = get_min_float()
-    
-    # normalization_function = lambda weight: math.log(100 - weight + min_float)
     normalization_function = lambda weight: 100 - weight
     
     new_graph = graph.duplicate()
     new_graph.normalize_graph_weights(normalization_function)
 
     return new_graph
-
-def get_min_float():
-    from sys import float_info
-    return float_info.min
 
 def shortest_path(graph, source_vertex, algorithm='dijkstra'):
     if algorithm == 'dijkstra':
